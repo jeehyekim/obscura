e139a68c63c9b11ef996065827ba6392b28c7060
jeehyekim <iam@jeehyekim.com>
2015-11-16 15:14:01 -0800

    created posts button; working on routes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,21 +1,26 @@
 class PostsController < ApplicationController
 
 	def new 
-		@city = City.find(params[:city_id])
-		@feature = Feature.find(params[:feature_id])
+		@city = City.find(params[:id])
+		# @feature = Feature.find(params[:feature_id])
 		@post = Post.new
 		render :new
 	end
 
 	def create
-		@post = Post.create(params.require(:post).permit(:title, :content, :imageUrl)
-		@city = City.find(params[:city_id])
+		@post = Post.create(params.require(:post).permit(:title, :content, :imageUrl))
+		@city = City.find(params[:id])
 		@post.city_id = @city.id
-		@feature = Feature.find(params[:feature_id])
+		# @feature = Feature.find(params[:feature_id])
 		@post.feature_id = @feature.id
 		@current_user = current_user
 		@post.user_id = @current_user.id
     @post.save
-		redirect_to @city
+		redirect_to post_path
 	end
+
+  def show
+    redirect_to city_path
+  end
+
 end
